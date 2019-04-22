3a4a861020a0beed6574067891d22777cf42dc36
Jacob Garcia <jacobgarcia@aggienetwork.com>
2019-04-22 03:12:31 -0500

    homepage content

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
 
 export default () =>  (
   <div className="Landing">
@@ -9,6 +11,16 @@ export default () =>  (
       <source src="assets/portfolio.ogg" type="video/ogg" />
       Your browser does not support the video tag.
     </video>
+
+      <div className="section">
+        <img alt="Jacob Garcia" src="./assets/jacob.jpg" />
+        <p><i className="fas fa-quote-left" />&nbsp;Hey there! My name's Jacob, and I'm a fullstack developer.&nbsp;<i className="fas fa-quote-right" /></p>
+        <Link to="about" className="btn">About</Link>
+      </div>
+      <div className="section">
+        <p><i className="fas fa-quote-left" />&nbsp;Jacob is the ideal team player...He truly brings out the best in his peers.&nbsp;<i className="fas fa-quote-right" /></p>
+        <Link to="reviews" className="btn">Reviews</Link>
+      </div>
 
   </div>
 )
