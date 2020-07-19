a63f09e0cefcd963226a32df41f5e8999589dc94
Jacob Garcia <jacobgarcia@aggienetwork.com>
2020-07-18 20:46:47 -0500

    updated about

--- src/components/About.js
+++ src/components/About.js
@@ -5,17 +5,22 @@ export default () =>  (
   <div className="About">
     <div className="section">
       <div className="center"><h2>Proficiencies</h2></div>
-      <p>JavaScript, jQuery, React, Redux, Node, Express, MongoDB, Mongoose, C#, HTML, CSS, Sass, Bootstrap, MySQL, Visual Studio, Adobe Creative Suite, etc.</p>
+      <p>Javascript, TypeScript, C#, Angular 2+, AngularJS, React, Redux, Node, Express, HTML, CSS, Sass, jQuery, Bootstrap, MongoDB, Mongoose, SQL, Firebase, Visual Studio, Adobe Creative Suite, Google Analytics, etc.</p>
     </div>
     <div className="section">
       <div className="center"><h2>Introduction</h2></div>
       <div>
-        <p>Hey there! My name's Jacob, and I'm a fullstack developer.</p>
-        <p>I currently work as a web development TA for a coding boot camp, where I help JavaScript, jQuery, React, etc. to UT students</p>
+        <p>Hey there! My name's Jacob, and I'm a software engineer.</p>
+        <p>Currently, I work as a UI/UX Angular developer at <a
+            href="https://www.kapsch.net/us/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >Kapsch TrafficCom</a>, where I work with a team to build and maintain tolling applications used by transportation agencies across the country.</p>
+        <p>I previously worked as a web development TA for a coding boot camp, where I helped teach JavaScript, jQuery, React, etc. to UT students</p>
         <p>I've been developing software for a long time&mdash;I taught myself at age 11! I would spend hours at a time growing up creating games and other applications with Visual Studio.</p>
         <p>I began getting into web develop during college. I graduated Texas A&amp;M University with an economics degree, and took several development classes while in college. </p>
         <p>Since then, I've continued to expand on my skills, taking courses through Udemy, while continuing to work on my own projects.</p>
-        <p>These days I do most of my coding in JavaScript, working with the React framework. Check out my <Link to="/portfolio">portfolio</Link> for samples of my recent work!
+        <p>These days I do most of my coding in JavaScript, working with the Angular and React frameworks. Check out my <Link to="/portfolio">portfolio</Link> for samples of my recent work!
         </p>
         <p>I'm really excited about my latest project&mdash;
           <a
